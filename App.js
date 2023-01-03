fd77f8288bf21babbc664b4b4eead2278438c972
Adrian Chojnacki <adrian.chojnacki@frontkom.com>
2023-01-03 21:44:32 +0100

    Add some styling

--- App.js
+++ App.js
@@ -1,11 +1,25 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { Alert, Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello World!</Text>
-      <StatusBar style="auto" />
+      <Text
+        style={{
+          marginBottom: 20,
+          borderWidth: 5,
+          borderColor: "red",
+          padding: 20,
+        }}
+      >
+        Piece of text
+      </Text>
+      <Text style={styles.text}>Hello World!</Text>
+      <TextInput placeholder="Placeholder..." style={styles.button} />
+      <Button
+        title="I'm a button"
+        onPress={() => Alert.alert("Simple Button pressed")}
+        style={styles.button}
+      />
     </View>
   );
 }
@@ -16,5 +30,12 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  text: {
+    fontSize: 40,
+  },
+  button: {
+    padding: 25,
+    fontSize: 40,
   },
 });
