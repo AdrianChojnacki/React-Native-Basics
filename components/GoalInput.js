57ddeb24997eb6e0a56304b677f864274d56bc81
Adrian Chojnacki <adrian.chojnacki@frontkom.com>
2023-01-12 19:14:03 +0100

    Added background and StatusBar

--- components/GoalInput.js
+++ components/GoalInput.js
@@ -35,10 +35,10 @@ function GoalInput(props) {
         />
         <View style={styles.buttonContainer}>
           <View style={styles.button}>
-            <Button title="Add Goal" onPress={addGoalHandler} />
+            <Button title="Add Goal" onPress={addGoalHandler} color="#b180f0" />
           </View>
           <View style={styles.button}>
-            <Button title="Cancel" onPress={props.onCancel} />
+            <Button title="Cancel" onPress={props.onCancel} color="#f31282" />
           </View>
         </View>
       </View>
@@ -62,10 +62,13 @@ const styles = StyleSheet.create({
     margin: 20,
   },
   textInput: {
-    borderWidth: 1,
-    borderColor: "#ccc",
     width: "100%",
-    padding: 8,
+    padding: 16,
+    backgroundColor: "#e4d0ff",
+    borderWidth: 1,
+    borderColor: "#e4d0ff",
+    borderRadius: 6,
+    color: "#120438",
   },
   buttonContainer: {
     flexDirection: "row",
